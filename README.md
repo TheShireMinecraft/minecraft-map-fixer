a0cd2b33c5dda72b40a925178e531bb005241cd0
Blitheness <19779808+Blitheness@users.noreply.github.com>
2025-10-02 01:00:00 +0100

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # minecraft-map-fixer
 Fix custom maps broken by updating to MC 1.21.8
+
+Broken maps caused errors like the following appearing in the server log:
+```
+[00:37:13 ERROR]: Error loading saved data: SavedDataType[map_16480]
+java.lang.NullPointerException: Cannot invoke "net.minecraft.nbt.Tag.asNumber()" because "tag" is null
+        at net.minecraft.nbt.NbtOps.getNumberValue(NbtOps.java:60)
+        at net.minecraft.nbt.NbtOps.getNumberValue(NbtOps.java:27)
+        at net.minecraft.resources.DelegatingOps.getNumberValue(DelegatingOps.java:51)
+        at com.mojang.serialization.Codec$13.read(Codec.java:582)
+        at com.mojang.serialization.codecs.PrimitiveCodec.decode(PrimitiveCodec.java:17)
+```
+
+This has been tested with PHP 8.3
+
+- `composer install`
+- Place maps in the `assets` directory
+    - So the assets directory contains files named similarly to `map_2345.dat`
+- Run the script `cd src && php index.php`
+- Check the `assets/fixed` directory for any maps the script attempted to fix
---
+++ src/index.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+use Aternos\Nbt\IO\Reader\GZipCompressedStringReader;
+use Aternos\Nbt\IO\Writer\GZipCompressedStringWriter;
+use Aternos\Nbt\NbtFormat;
+use Aternos\Nbt\Tag\CompoundTag;
+use Aternos\Nbt\Tag\Tag;
+use Aternos\Nbt\Tag\TagType;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+
+require '../vendor/autoload.php';
+
+$logger = new Logger('MinecraftMapFixer');
+$logger->pushHandler(new StreamHandler('../logs/minecraft-map-fixer.log'));
+
+$directoryToScan = '../assets/';
+$outputDirectory = '../assets/fixed';
+
+// Scan the directory for map_*.dat files and fix each one
+$paths = glob($directoryToScan . 'map_*.dat');
+foreach ($paths as $path) {
+    fixMapFile($path, $outputDirectory, $logger);
+}
+
+// Broken maps have a data version >4440 and a dimension tag that is not a string
+function fixMapFile(
+    string $path,
+    string $outputDirectory,
+    LoggerInterface $logger
+): void
+{
+    $file = file_get_contents($path);
+
+    /** @noinspection PhpUnhandledExceptionInspection */
+    $reader = new GZipCompressedStringReader(
+        $file,
+        NbtFormat::JAVA_EDITION
+    );
+
+    /** @noinspection PhpUnhandledExceptionInspection */
+    $rootTag = Tag::load($reader);
+
+    if (!($rootTag instanceof CompoundTag)) {
+        $logger->error("Root tag is not a compound tag", [
+            'path' => $path,
+            'rootTagType' => get_class($rootTag)
+        ]);
+        exit;
+    }
+
+    $dataVersion = $rootTag->getInt("DataVersion")?->getValue() ?? 0;
+
+    if ($dataVersion <= 4440) {
+        $logger->info("No fix needed for this data version", [
+            'path' => $path,
+            'dataVersion' => $dataVersion
+        ]);
+        exit;
+    }
+
+    $data = $rootTag->getCompound("data");
+
+    if (is_null($data)) {
+        $logger->error("No data tag found", [
+            'path' => $path,
+        ]);
+    }
+
+    $dimension = $data->get("dimension");
+
+    if ($dimension->getType() === TagType::TAG_String) {
+        exit;
+    }
+
+    // At this point, dataVersion is >4440 and dimension is not a string
+
+    // Set the data version to 1343
+    $dataVersionTag = $rootTag->getInt("DataVersion");
+    $dataVersionTag->setValue(1343);
+
+    // Save the corrected map
+    $writer = new GZipCompressedStringWriter();
+    $rootTag->write($writer);
+    $newFileContent = $writer->getStringData();
+
+    $fileName = basename($path);
+    $outPath = $outputDirectory . '/' . $fileName;
+    if (file_put_contents($outPath, $newFileContent) === false)
+    {
+        $logger->error("Unable to write fixed file", [
+            'target' => $outPath,
+        ]);
+        exit;
+    }
+
+    $logger->info("Successfully fixed map file", [
+        'fileName' => $fileName,
+        'newDataVersion' => $dataVersionTag->getValue()
+    ]);
+}
